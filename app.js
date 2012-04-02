be4fb25b81571d8b0358007c4bf28113671fd72c
Kyle Partridge <partkyle@gmail.com>
2012-04-01 17:57:03 -0700

    added a dummy version route

--- app.js
+++ app.js
@@ -31,5 +31,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
+app.get('/version', routes.version);
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.version = function(req, res){
+  res.json({version: '1.0'})
+};
